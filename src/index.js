5ae483d9184704a494692c65d995ad456f6f5d1c
Key Feula <keyfeula1214@gmail.com>
2025-03-13 17:42:35 -0700

    Add dom controller object to handle click events and update display

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import "./style.css";
 
-const dialog = document.querySelector("dialog").showModal();
+const domController = (() => {
+    const dialog = document.querySelector("dialog");
+    const body = document.querySelector("body");
+    body.addEventListener("click", (event) => {
+        const target = event.target;
+        if (target.tagName !== "BUTTON") {
+            return;
+        }
+        if (target.classList.contains("new-btn")) {
+            dialog.showModal();
+        }
+        if (target.classList.contains("form-close-btn")) {
+            dialog.close();
+        }
+    });
+})();
