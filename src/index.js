f6d872789545f75d83ae8e27880be58324b393ed
Key Feula <keyfeula1214@gmail.com>
2025-03-14 19:02:08 -0700

    Add event listener function for handling new project button click

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import "./style.css";
+import { TodoProject } from "./todo-project";
 
 const domController = (() => {
     const dialog = document.querySelector("dialog");
     const body = document.querySelector("body");
+    const projectsContainer = document.querySelector(".projects");
+    const projects = [];
+
+    function updateProjectsDisplay() {
+        projectsContainer.textContent = "";
+        for (let i = 0; i < projects.length; i++) {
+            const newProjectBtn = document.createElement("button");
+            newProjectBtn.classList.add("project");
+            newProjectBtn.textContent = projects[i].name;
+            projectsContainer.append(newProjectBtn);
+        }
+        const addProjectBtn = document.createElement("button");
+        addProjectBtn.classList.add("new-btn");
+        addProjectBtn.textContent = "+";
+        projectsContainer.append(addProjectBtn);
+    }
+
     body.addEventListener("click", (event) => {
         const target = event.target;
         if (target.tagName !== "BUTTON") {
@@ -11,8 +29,15 @@ const domController = (() => {
         if (target.classList.contains("new-btn")) {
             dialog.showModal();
         }
-        if (target.classList.contains("form-close-btn")) {
+        else if (target.classList.contains("form-close-btn")) {
             dialog.close();
         }
+        else if (target.classList.contains("project-submit-btn")) {
+            const projectName = document.querySelector("input#project-name").value;
+            const todoProject = new TodoProject(projectName);
+            projects.push(todoProject);
+            updateProjectsDisplay();
+        }
+
     });
 })();
