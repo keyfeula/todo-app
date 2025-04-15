215d84cf762ccec7120637eb00b412ad11777a26
Key Feula <keyfeula1214@gmail.com>
2025-04-15 16:50:55 -0700

    Add function to update projects display container

--- src/index.js
+++ src/index.js
@@ -3,7 +3,20 @@ import { logicController } from "./logic-controller";
 
 logicController.addProject("proj 1");
 logicController.addProject("proj 2");
+logicController.addProject("project 3");
 
-for (let i = 0; i < logicController.getLength(); i++) {
-    console.log(logicController.getProjectAt(i).getName());
+const projectsContainer = document.querySelector(".projects-container");
+
+function updateProjectDisplay() {
+    for (let i = 0; i < logicController.getLength(); i++) {
+        const project = document.createElement("li");
+        const projectButton = document.createElement("button");
+        projectButton.classList.add("project-button");
+        projectButton.setAttribute("type", "button");
+        projectButton.textContent = logicController.getProjectAt(i).getName();
+        project.append(projectButton);
+        projectsContainer.append(project);
+    }
 }
+
+updateProjectDisplay();
