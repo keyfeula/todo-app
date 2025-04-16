ec8641bbdd34cd7c7075f3694ed0d181654d4f03
Key Feula <keyfeula1214@gmail.com>
2025-04-15 17:47:50 -0700

    Add event listener to display name of current project in todo list container header

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import "./style.css";
 import { logicController } from "./logic-controller";
 
-logicController.addProject("proj 1");
-logicController.addProject("proj 2");
+logicController.addProject("project 1");
+logicController.addProject("project 2");
 logicController.addProject("project 3");
 
+const pageBody = document.querySelector("body");
 const projectsContainer = document.querySelector(".projects-container");
+const todosHeader = document.querySelector(".todos-header");
 
 function updateProjectDisplay() {
     for (let i = 0; i < logicController.getLength(); i++) {
@@ -13,10 +15,19 @@ function updateProjectDisplay() {
         const projectButton = document.createElement("button");
         projectButton.classList.add("project-button");
         projectButton.setAttribute("type", "button");
+        projectButton.setAttribute("id", `project-${i}`);
         projectButton.textContent = logicController.getProjectAt(i).getName();
         project.append(projectButton);
         projectsContainer.append(project);
     }
 }
+
+pageBody.addEventListener("click", (event) => {
+    const target = event.target;
+    if (target.classList.contains("project-button")) {
+        logicController.setCurrentProjectIndex(target.id.slice(-1));
+        todosHeader.textContent = target.textContent;
+    }
+});
 
 updateProjectDisplay();
--- src/logic-controller.js
+++ src/logic-controller.js
@@ -2,11 +2,13 @@ import { Project } from "./project";
 import { Todo } from "./todo";
 
 const projects = [];
+let currentProjectIndex = 0;
 
 export const logicController = {
     addProject(name) {
         const newProject = new Project(name);
         projects.push(newProject);
+        currentProjectIndex = projects[projects.length - 1];
     },
 
     getProjectAt(index) {
@@ -15,5 +17,13 @@ export const logicController = {
 
     getLength() {
         return projects.length;
+    },
+
+    getCurrentProjectIndex() {
+        return currentProjectIndex;
+    },
+
+    setCurrentProjectIndex(newIndex) {
+        currentProjectIndex = newIndex;
     }
 }
